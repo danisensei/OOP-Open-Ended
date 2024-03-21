6815c2fe07cb9fd92f4bdd18401a099702ea22a3
Malik Daniyal Ahmad <146337576+danisensei@users.noreply.github.com>
2024-03-21 06:14:04 +0000

    Code Updared, Functions and attributes added for student

--- OpenEnded.cpp
+++ OpenEnded.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
+#include<vector>
 #include<string>
 using namespace std;
 
 class Student
 {
+    private:
+    string studentID;
+    string name;
+    string email;
+    vector<string> coursesEnrolled;
+
+    public:
+    Student(string id, string n, string mail) : studentID(id), name(n), email(mail) {}
+
+    void enrollaCourse(string course){}
+    void dropaCourse(string course) {}
+    void viewCourses() {}
   
 };
 
